3129c4d06f59023fb2cbbbbcc98c9cd8c04919ff
nainghtetwai-anbuchi <nainghtetwai.anbuchi@gmail.com>
2023-11-09 16:34:15 +0630

    mobile layout changes

--- src/components/NewSongs.tsx
+++ src/components/NewSongs.tsx
@@ -12,18 +12,27 @@ const NewSongs = () => {
       <SectionLayout>
         <div className="mb-6 flex justify-between">
           <div className="text-2xl  font-medium">New Songs</div>
-          <Link href={"/new-songs"}>
-            <RippleBtn classnames="rounded-[10px] bg-grey px-3 py-1 font-medium text-primary-light">
-              More
-            </RippleBtn>
-          </Link>
+          <div className="hidden xs:block">
+            <Link href={"/new-songs"}>
+              <RippleBtn classnames="rounded-[10px] bg-grey px-3 py-1 font-medium text-primary-light">
+                More
+              </RippleBtn>
+            </Link>
+          </div>
         </div>
-        <div className="mb-[40px] grid grid-cols-1 place-items-center gap-6 xs:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+        <div className="mb-[40px] grid grid-cols-3 place-items-center gap-6  lg:grid-cols-4 xl:grid-cols-5">
           {cards.map((el, idx) => (
             <div key={idx}>
               <Card />
             </div>
           ))}
+        </div>
+        <div className="mb-4 block w-fit scale-[0.8] xs:hidden">
+          <Link href={"/new-songs"}>
+            <RippleBtn classnames="rounded-[10px] bg-grey px-3 py-1 font-medium text-primary-light">
+              More
+            </RippleBtn>
+          </Link>
         </div>
         <div className="underlined h-[2px] w-full"></div>
       </SectionLayout>
