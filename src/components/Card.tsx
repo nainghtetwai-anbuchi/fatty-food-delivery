4c660812047e4b92a465183f42131a3421cb5061
nainghtetwai-anbuchi <nainghtetwai.anbuchi@gmail.com>
2023-11-09 12:02:39 +0630

    refactor text-align

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,14 +8,13 @@ const Card = () => {
       <div className="absolute right-4 top-4 flex h-8 w-8 cursor-pointer items-center justify-center rounded-full border border-primary-light bg-[#1a1a1a]">
         <PlayIcon />
       </div>
-      <img
-        src={"/ed-sheerean.png"}
-        className="mb-3 max-h-[80%] w-full object-cover"
-      />
-      <div className="mb-2 text-center font-medium text-primary-dark">
+      <div className="max-h-[221px] max-w-[221px] border border-green-500">
+        <img src={"/ed-sheerean.png"} className="h-full w-full object-cover" />
+      </div>
+      <div className="mb-2 self-start font-medium text-primary-dark">
         Shape Of You
       </div>
-      <div className="text-center text-grey ">Ed Sheeran</div>
+      <div className="self-start text-grey ">Ed Sheeran</div>
     </div>
   );
 };
