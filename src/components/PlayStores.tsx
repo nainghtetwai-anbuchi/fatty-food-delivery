82a2dd2d28739c7cb9f2c53786f853e2a363e6ad
nainghtetwai-anbuchi <nainghtetwai.anbuchi@gmail.com>
2023-11-09 15:48:27 +0630

    make playstores component responsive

--- src/components/PlayStores.tsx
+++ src/components/PlayStores.tsx
@@ -7,20 +7,20 @@ const PlayStores = () => {
   return (
     <>
       <SectionLayout>
-        <div className="flex justify-between bg-black p-8">
+        <div className="flex flex-col items-center justify-between gap-6 bg-black p-8 md:flex-row">
           <div>
             <div className="mb-2 font-medium">
               Best Ways To Listen To Music!
             </div>
             <div className="text-grey-dark">
               Don't forget to install
-              <span className="mx-2 text-primary-dark">
+              <span className="mx-1 text-primary-dark">
                 SongLink Music Application{" "}
               </span>{" "}
               on your mobile phone.
             </div>
           </div>
-          <div className="flex gap-4">
+          <div className=" flex  flex-col items-center justify-center gap-2  sm:flex-row sm:justify-start sm:gap-4">
             <StoreIcon storeName="App Store">
               <AppStoreIcon />
             </StoreIcon>
