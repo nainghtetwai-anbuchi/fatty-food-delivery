17a72f88a66772f23f562589dca517def6448a02
nainghtetwai-anbuchi <nainghtetwai.anbuchi@gmail.com>
2023-11-09 11:44:22 +0630

    play button added to Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
 import React from "react";
+import PlayIcon from "../icons/play-button.svg";
 
 const Card = () => {
   return (
-    <div className="flex flex-col items-center ">
+    <div className="relative flex flex-col items-center ">
+      <div className="absolute right-4 top-4 flex h-8 w-8 cursor-pointer items-center justify-center rounded-full border border-primary-light bg-[#1a1a1a]">
+        <PlayIcon />
+      </div>
       <img
         src={"/ed-sheerean.png"}
         className="mb-3 max-h-[80%] w-full object-cover"
