ef9f96f6c94c395ff5738b8fed023e64363ce0a4
nainghtetwai-anbuchi <nainghtetwai.anbuchi@gmail.com>
2023-11-09 10:09:48 +0630

    refactor padding and margin

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -10,7 +10,7 @@ const HeroSection = () => {
   const { ref } = useInView({ threshold: 0.2 });
 
   return (
-    <div className="hero-section-bg px-4 " id="hero">
+    <div className="hero-section-bg px-8 " id="hero">
       <SectionLayout>
         <div ref={ref} className="flex min-h-[430px] " id="home">
           <div className=" mt-auto flex w-full flex-col-reverse sm:flex-row">
@@ -28,7 +28,7 @@ const HeroSection = () => {
                   Explore a vast library of tracks and connect with like-minded
                   music enthusiasts from around the globe.
                 </div>
-                <div className=" flex xs:gap-2 sm:gap-4">
+                <div className=" flex flex-col items-center justify-center gap-2 xs:flex-row sm:gap-4">
                   <StoreIcon storeName="App Store">
                     <AppStoreIcon />
                   </StoreIcon>
@@ -39,7 +39,7 @@ const HeroSection = () => {
               </div>
             </div>
 
-            <div className="relative mb-6 h-[280px] sm:mb-0 sm:h-[430px] sm:flex-1">
+            <div className="relative mb-0 h-[280px] xs:mb-6 sm:mb-0 sm:h-[430px] sm:flex-1">
               <Image
                 src={"/ts-half.png"}
                 fill
@@ -51,54 +51,11 @@ const HeroSection = () => {
                 quality={100}
               />
             </div>
-            <div className="mb-4 text-2xl font-medium text-black sm:hidden">
+            <div className="mb-0 mt-2 text-2xl font-medium text-black xs:mb-2 sm:hidden">
               Let me connect your heart to beats!
             </div>
           </div>
         </div>
-
-        {/* <div className="flex h-full w-full flex-col justify-between border border-green-500 sm:flex-row ">
-            <div className="flex flex-1 items-center justify-center ">
-              <div className="min-w-[240px] w-[80%]">
-                <div className="mb-4 text-2xl font-medium text-black">
-                  Let me connect your heart to beats!
-                </div>
-                <div className="mb-6 text-2xl font-bold">
-                  SongLink music application
-                </div>
-                <div className="mb-8">
-                  Discover a world of musical possibilities with our music
-                  application, where users and songs unite in perfect harmony.
-                  Explore a vast library of tracks and connect with like-minded
-                  music enthusiasts from around the globe.
-                </div>
-                <div className=" flex xs:gap-2 sm:gap-4">
-                  <StoreIcon storeName="App Store">
-                    <AppStoreIcon />
-                  </StoreIcon>
-                  <StoreIcon storeName="Play Store">
-                    <GooglePlayIcon />
-                  </StoreIcon>
-                </div>
-              </div>
-            </div>
-            <div className="flex-1 self-stretch"></div>
-
-            <div className=" flex flex-1 border border-blue-700 ">
-              <div className=" relative  w-full ">
-                <Image
-                  src={"/ts-half.png"}
-                  fill
-                  sizes="(min-width: 808px) 50vw, 100vw"
-                  style={{
-                    objectFit: "contain", // cover, contain, none
-                  }}
-                  alt="ts-half"
-                />
-              </div>
-            </div>
-          </div> 
-        </div>*/}
       </SectionLayout>
     </div>
   );
