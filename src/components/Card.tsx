45b61e64758557d97e0fcbb08e783ddc41e50bbb
nainghtetwai-anbuchi <nainghtetwai.anbuchi@gmail.com>
2023-11-10 09:29:41 +0630

    size changes for play icon

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,7 +5,7 @@ import PlayIcon from "../icons/play-button.svg";
 const Card = () => {
   return (
     <div className="relative flex flex-col items-center text-[8px] sm:text-base ">
-      <div className="absolute right-0 top-0 flex h-8 w-8 scale-[0.7]  cursor-pointer items-center justify-center rounded-full border border-primary-light bg-[#1a1a1a] sm:right-2 sm:top-2 sm:scale-[1] ">
+      <div className="absolute -right-2 -top-2 flex h-8 w-8 scale-[0.4] cursor-pointer items-center justify-center  rounded-full border border-primary-light bg-[#1a1a1a] xs:right-0 xs:top-0 xs:scale-[0.7] sm:right-2 sm:top-2 sm:scale-[1] ">
         <PlayIcon />
       </div>
       <div className="mb-3 max-h-[221px] max-w-[221px]">
